683052a0bfce26b33431fc42a177f41c81b40663
thevalleyy <74456686+thevalleyy@users.noreply.github.com>
2022-02-14 18:21:23 +0100

    Merge branch 'master' of https://github.com/thevalleyy-NetWork/thevalleyy-Bot

--- content/log_nickname.js
+++ content/log_nickname.js
@@ -10,6 +10,12 @@ module.exports = async(client) => {
             dynamic: true
         })
 
+        const fetchedLogs = await newMember.guild.fetchAuditLogs({
+            limit: 1,
+            type: 'MEMBER_UPDATE'
+        })
+        const latest = fetchedLogs.entries.first()
+        const executor = latest.executor
         if (newMember.nickname === null) {
             var oldname = oldMember.nickname
             var newname = newMember.user.username
@@ -27,18 +33,10 @@ module.exports = async(client) => {
             .addField('User:', `${newMember.user.tag}, <@!${newMember.user.id}>`)
             .addField("Vorher: ", `\`${oldname}\``, true)
             .addField("Nachher: ", `\`${newname}\``, true)
-            .addField("Ausgeführt von: ", "Soon", true)
+            .addField("Ausgeführt von: ", `<@!${executor.id}>`, true)
             .setFooter(newMember.guild.name, iconurl)
             .setTimestamp()
             .setColor('#24E498')
         client.channels.cache.get(botlog).send({ embeds: [embedNickLog] })
-
-
-
-
-        // send logmessage
-
-
-
     })
 }
