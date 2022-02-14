3068d619e6110b934e50d892ec3410d6b7587bb7
thevalleyy <74456686+thevalleyy@users.noreply.github.com>
2022-02-14 18:26:15 +0100

    fixed some more stuff

--- content/welcome.js
+++ content/welcome.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js')
 const channelId = '786239944581840956'
 module.exports = client => {
     client.on('guildMemberAdd', member => {
+        if (member.guild.id != '631518992342843392') return
         const message = `Herzlich Willkommen, <@${member.id}> auf dem **thevalleyy-NetWork**. \nLese dir noch das <#786239847554875402> durch, dann kannst du loslegen! :D`
 
         const channel = member.guild.channels.cache.get(channelId)
