8ab3e3d1fb724f148f0f6633f6c57c80ddb2cde2
thevalleyy <74456686+thevalleyy@users.noreply.github.com>
2022-03-29 17:06:28 +0200

    only load js files

--- main.js
+++ main.js
@@ -77,10 +77,14 @@ client.on('ready', async() => {
         const files = fs.readdirSync(path.join(__dirname, dir))
         for (const file of files) {
             const stat = fs.lstatSync(path.join(__dirname, dir, file))
-            console.log(`[${gettime()}] » Loading: ${file}`)
             if (stat.isDirectory()) {
                 readCommands(path.join(dir, file))
             } else if (file !== baseFile) {
+                if (!file.endsWith('.js')) {
+                    console.log(`[${gettime()}] » Skipping file ${file}`)
+                    return
+                }
+                console.log(`[${gettime()}] » Loading: ${file}`)
                 const option = require(path.join(__dirname, dir, file))
                 commandBase(client, option)
             }
@@ -95,6 +99,10 @@ client.on('ready', async() => {
             return console.log('[' + gettime() + '] » Error: Unable to scan directory: ' + err)
         }
         files.forEach(function(file) {
+            if (!file.endsWith('.js')) {
+                console.log(`[${gettime()}] » Skipping file ${file}`)
+                return
+            }
             require('./content/' + file)(client)
             console.log(`[${gettime()}] » Loading: ${file}`)
         })
