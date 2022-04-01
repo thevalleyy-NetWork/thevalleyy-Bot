50262caf41b638247dfa8c0db1e3310cb4e1712a
thevalleyy <74456686+thevalleyy@users.noreply.github.com>
2022-04-01 22:09:10 +0200

    this is not working but i comit it anyway

--- commands/cmds/cooldown.js
+++ commands/cmds/cooldown.js
@@ -10,6 +10,26 @@ module.exports = {
         var iconurl = message.guild.iconURL({ dynamic: true })
 
 
+        module.exports = (client, commandOptions) => {
+            let {
+                commands,
+                expectedArgs = '',
+                permissionError = 'Unzureichende Berechtigungen',
+                minArgs = 0,
+                maxArgs = null,
+                cooldown = null,
+                description = 'Well, this is a description... What did you expect?',
+                permissions = [],
+                requiredRoles = [],
+                callback
+            } = commandOptions
+
+            for (const alias of commands) {
+                console.log(alias)
+            }
+        }
+
+
     },
     permissions: [],
     requiredRoles: ['Nice One']
