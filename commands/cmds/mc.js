e5b136e44b35187a26840416abea8d94bc27b4d0
thevalleyy <74456686+thevalleyy@users.noreply.github.com>
2022-05-25 19:24:22 +0200

    mc cmd supports custom servers

--- commands/cmds/mc.js
+++ commands/cmds/mc.js
@@ -1,55 +1,66 @@
+const fs = require('fs')
+const config = require('./../../config.json')
+const util = require('minecraft-server-util')
+const Discord = require('discord.js')
+const playerCount = fs.readFileSync("./data/playerrec.json", 'utf8');
+const playerCount_ = JSON.parse(playerCount)
+const modlog = '822575095721099304'
+
 module.exports = {
     commands: ['mc', 'minecraft'],
     expectedArgs: '[IP] [Port]',
     permissionError: '',
     minArgs: 0,
     maxArgs: 2,
-    cooldown: 30000,
+    cooldown: 10000,
     description: "this description is weird",
     callback: (message, arguments, text) => {
 
-        var iconurl = message.guild.iconURL({
-            dynamic: true
-        })
-        const fs = require('fs')
-        const util = require('minecraft-server-util')
-        const Discord = require('discord.js')
-        const playerCount = fs.readFileSync("./data/playerrec.json", 'utf8');
-        const playerCount_ = JSON.parse(playerCount)
-        const modlog = '822575095721099304'
+        if (!arguments[0] && !arguments[1]) {
+            arguments[0] = "thevalleyy.tk"
+            arguments[1] = "25504"
+        }
+        if (!arguments[1]) {
+            arguments[1] = "25565"
+        }
 
+        util.status(arguments[0], parseInt(arguments[1])).catch(error => {
 
-        try {
-            util.status('node2.chaosbothosting.de', 25504)
-                .then(async(response) => {
-                    const embedSuccess = new Discord.MessageEmbed()
-                        .setTitle("thevalleyy-NetWork")
-                        .setFooter("thevalleyy NetWork", iconurl)
-                        .setDescription("IP: thevalleyy.tk")
-                        .setColor("#16f491")
-                        .setThumbnail("https://eu.mc-api.net/v3/server/favicon/thevalleyy.tk")
-                        .addField("Spieler:", `(${response.players.online}/${response.players.max})`, true)
-                        .addField("Spielerrekord:", playerCount_.mostPlayers, true)
-                        .addField("Erreicht am:", playerCount_.date, true)
-                        .addField("Letzter Ping:", playerCount_.lastPinged, true)
-                        .addField("Version:", response.version.name, true)
-
-                    await message.reply({ embeds: [embedSuccess] })
-
-                })
-        } catch (error) {
             const failEmbed = new Discord.MessageEmbed()
                 .setTitle('Es gab einen Fehler bei -mc')
                 .setThumbnail('https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/ab0c1e57515093.59d8c6eb16d19.gif')
                 .setDescription('Fehler: `' + error + '`')
                 .addField(message.author.tag, 'in <#' + message.channel.id + '>')
-                .setFooter('thevalleyy-NetWork', iconurl)
+                .setFooter({ text: message.guild.name, iconURL: message.guild.iconURL({ dynamic: true }) })
                 .setTimestamp()
                 .setColor('fc036b')
-            message.client.channels.cache.get(modlog).send({ embed: failEmbed })
-            message.reply("Es gab einen Fehler...")
 
-        }
+            message.client.channels.cache.get(modlog).send({ embeds: [failEmbed] })
+        }).then(async(response) => {
+
+            if (!response) return message.reply("Der Server ist nicht erreichbar.")
+
+            const embed = new Discord.MessageEmbed()
+                .setTitle(arguments[0] + ":" + arguments[1])
+                .setTimestamp()
+                .setColor(config.standard_color)
+                .setFooter({ text: message.guild.name, iconURL: message.guild.iconURL({ dynamic: true }) })
+                .addField("Spieler:", `(${response.players.online}/${response.players.max})`, true)
+                .addField("Version:", `${response.version.name} (${response.version.protocol})`, true)
+                .addField("Ping:", response.roundTripLatency.toString() + "ms", true)
+                .setThumbnail("https://eu.mc-api.net/v3/server/favicon/" + arguments[0] + ":" + arguments[1])
+                .setImage(`http://status.mclive.eu/${arguments[0]}/${arguments[0]}/${arguments[1]}/banner.png`)
+
+
+            if (arguments[0] == "thevalleyy.tk") {
+                embed.addField("Spielerrekord:", playerCount_.mostPlayers, true)
+                embed.addField("Erreicht am:", playerCount_.date, true)
+                embed.addField("Letzter Ping:", playerCount_.lastPinged, true)
+            }
+
+            await message.reply({ embeds: [embed] })
+
+        })
     },
     permissions: [],
     requiredRoles: ['Nice One']
