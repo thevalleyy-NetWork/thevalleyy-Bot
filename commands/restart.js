2a6fba58a1899d338707208cf1d1474fe484ee5e
thevalleyy <74456686+thevalleyy@users.noreply.github.com>
2022-08-29 14:51:29 +0200

    half finished restart command

--- commands/restart.js
+++ commands/restart.js
@@ -1,21 +1,37 @@
-        module.exports = {
-            commands: ['restart'],
-            expectedArgs: '',
-            permissionError: 'Diese Nachricht sollte es nie geben',
-            minArgs: 0,
-            maxArgs: 0,
-            cooldown: null,
-            description: "Lädt den Bot neu",
-            callback: async(message, arguments, text) => {
+const process = require('process');
+const { spawn } = require('child_process');
 
-                // const iconurl = message.guild.iconURL()
-                // const modlog = '822575095721099304'
-                // const Discord = require('discord.js')
+module.exports = {
+    commands: ['restart'],
+    expectedArgs: '',
+    permissionError: 'Diese Nachricht sollte es nie geben',
+    minArgs: 0,
+    maxArgs: 0,
+    cooldown: null,
+    description: "Lädt den Bot neu",
+    callback: async(message, arguments, text) => {
+        // const iconurl = message.guild.iconURL()
+        // const modlog = '822575095721099304'
 
-                await message.reply("Neustart eingeleitet.")
-                await process.exit(42)
-                // TODO: richtiger restart, nicht nur den process exitten
-            },
-            permissions: [],
-            requiredRoles: ['Moderator']
-        }
+            (function main() {
+              if (process.env.process_restarting) {
+                delete process.env.process_restarting;
+                // Give old process one second to shut down before continuing ...
+                setTimeout(main, 1000);
+                return;
+              }
+          
+              // Restart process ...
+              spawn(process.argv[0], process.argv.slice(1), {
+                env: { process_restarting: 1 },
+                stdio: 'ignore',
+                detached: true
+              }).unref();
+            })();
+
+            process.exit(1)
+
+    },
+    permissions: [],
+    requiredRoles: ['thevalleyy']
+}
